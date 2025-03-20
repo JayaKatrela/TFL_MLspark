ffd6e998b5ff8a4480b01d503662c574a82d31fc
JayaKatrela <nagalakshmi.k95@gmail.com>
2025-03-20 22:53:58 +0000

    Update test_docker.sh

--- kubeml/test/test_docker.sh
+++ kubeml/test/test_docker.sh
@@ -6,7 +6,7 @@ echo "📦 Building Docker image..."
 docker build -t $IMAGE_NAME kubeml/
 
 echo "🚢 Running Docker tests..."
-docker run --rm $IMAGE_NAME python3 -m unittest discover -s /app/test -p "test_pyspark.py"
+docker run --rm $IMAGE_NAME python3 -m unittest discover -s /app/test -p "test_forecasteml.py"
 
 if [ $? -eq 0 ]; then
     echo "✅ Docker tests passed!"
